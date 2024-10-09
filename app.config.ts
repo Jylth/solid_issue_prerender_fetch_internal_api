040f40955f26051b78db554ba26b1b751fe3e9c1
Oumazzan Nadem <oumazzan@gmail.com>
2024-10-09 21:03:18 +0200

    update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -2,14 +2,10 @@ import { defineConfig } from '@solidjs/start/config'
 
 export default defineConfig({
   server: {
-    preset: 'cloudflare-pages',
     prerender: {
       failOnError: true,
       crawlLinks: false,
       routes: ['/'],
-    },
-    cloudflare: {
-      pages: {},
     },
   },
 })
